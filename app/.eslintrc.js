1e9b3c8a1b4228ac6436ea4c3640ed48b9030ca9
Wito Delnat <wito.delnat@inthepocket.mobi>
2021-03-06 19:59:08 +0100

    chore(app): change eslint rules

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -3,6 +3,8 @@ module.exports = {
   rules: {
     // A temporary hack related to IDE not resolving correct package.json
     'import/no-extraneous-dependencies': 'off',
+    '@typescript-eslint/no-use-before-define': ['error', { functions: false }],
+    'import/prefer-default-export': 'off',
   },
   parserOptions: {
     ecmaVersion: 2020,
